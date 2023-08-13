7ffabc7d7bffeabbe7c7fa1fd0d6a18a34ceae3e
infandis <svetlana290699@gmail.com>
2023-08-13 15:20:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
+const validation = Yup.object().shape({
+  name: Yup.string().required("Це поле є обов'язковим"),
+  email: Yup.string().email('Введіть коректну електронну пошту').required("Це поле є обов'язковим"),
+  phone: Yup.string().matches(/^\d{12}$/, 'Телефон повинен містити лише 12 цифр').required("Це поле є обов'язковим"),
+});
+
+const App = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Форма</h1>
+      <Formik
+        initialValues={{ name: '', email: '', phone: '' }}
+        validation={validation}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">Ім'я</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component="div" className="error" />
+          </div>
+
+          <div>
+            <label htmlFor="email">Електронна пошта</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+
+          <div>
+            <label htmlFor="phone">Телефон</label>
+            <Field type="text" id="phone" name="phone" />
+            <ErrorMessage name="phone" component="div" className="error" />
+          </div>
+
+          <button type="submit">Відправити</button>
+        </Form>
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
+
+
